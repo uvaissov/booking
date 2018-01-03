2eba53bd4ab4e680fb00dcf18eb70f2c66472cb3
uvaissov <uvaissov@gmail.com>
2018-01-03 23:41:11 +0600

    1

--- src/main/java/kz/astana/uvaissov/booking/configure/WebSecurityConfig.java
+++ src/main/java/kz/astana/uvaissov/booking/configure/WebSecurityConfig.java
@@ -12,12 +12,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests()
-				.anyRequest().fullyAuthenticated()
-				.and()
-			.formLogin()
-			.loginPage("/login").permitAll()
-			;
+        .authorizeRequests()
+            .antMatchers("/resources/**", "/registration").permitAll()
+            .anyRequest().authenticated()
+            .and()
+        .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .and()
+        .logout()
+            .permitAll();
 	}
 
 	@Override
--- src/main/java/kz/astana/uvaissov/booking/controller/IndexController.java
+++ src/main/java/kz/astana/uvaissov/booking/controller/IndexController.java
@@ -1,20 +1,37 @@
 package kz.astana.uvaissov.booking.controller;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+import kz.astana.uvaissov.booking.model.User;
+
+@Controller
 public class IndexController {
 
-	@GetMapping("/")
-    public ModelAndView index() {
-		Map<String, String> model = new HashMap<>();
-        model.put("name", "Alexey");
-        return new ModelAndView("index", model);
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Ваш логин или пароль не верны");
+
+        if (logout != null)
+            model.addAttribute("message", "Вы вышли");
+
+        return "login";
+    }
+
+    @RequestMapping(value = {"/","/workspace"}, method = RequestMethod.GET)
+    public String workspace(Model model) {
+    	
+        return "workspace";
+    }
+    
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+        return "registration";
     }
 }
