c224c9b2c42f61784f88e9fad1532e64d29d2023
uvaissov <uvaissov@gmail.com>
2018-01-25 08:05:57 +0600

    init thymleaf

---
+++ src/main/java/kz/astana/uvaissov/booking/controller/IndexController.java
@@ -0,0 +1,40 @@
+package kz.astana.uvaissov.booking.controller;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import kz.astana.uvaissov.booking.model.User;
+import kz.astana.uvaissov.booking.service.UserService;
+
+@Controller
+public class IndexController {
+
+	@Autowired
+	private UserService userService;
+	
+
+    @RequestMapping(value = {"/index","/workspace"}, method = RequestMethod.GET)
+    public ModelAndView workspace(Model model) {
+    	ModelAndView modelAndView = new ModelAndView();
+    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    	User user = userService.findUserByEmail(auth.getName());
+    	modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
+		modelAndView.setViewName("index");
+		return modelAndView;
+    }
+    
+   
+}
