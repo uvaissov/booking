b6aaf6e3118fd6c04facc605f133f35844ee590e
uvaissov <uvaissov@gmail.com>
2018-01-26 00:05:17 +0600

    1

--- src/main/java/kz/astana/uvaissov/booking/controller/IndexController.java
+++ src/main/java/kz/astana/uvaissov/booking/controller/IndexController.java
@@ -1,15 +1,9 @@
 package kz.astana.uvaissov.booking.controller;
 
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
