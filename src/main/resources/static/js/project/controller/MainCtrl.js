596be43470111f904f0c6f0308bf7b9d9264bf6e
Serik Uvaissov <suvaissov@networkenergy.net>
2018-01-26 12:02:22 +0600

    add main.page

--- src/main/resources/static/js/project/controller/MainCtrl.js
+++ src/main/resources/static/js/project/controller/MainCtrl.js
@@ -4,6 +4,7 @@ function MainCtrl () {
   var main = this; 
   main.title = 'Developer';
   main.navigationList = [] ;
+  alert(/*[[${userName}]]*/);
   main.changeContent=function(item){
 	  main.title =  item.currentTarget.attributes['data-page'].value;
   }
